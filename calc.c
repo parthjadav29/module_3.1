5fb8d56e11fbe363e2edf4a9d8bfaf381584604d
parthjadav29 <parthjadav@hotmail.com>
2022-08-19 23:21:33 +0530

    calc program

---
+++ calc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+
+    int a;
+    int b;
+    printf("enter two value");
+    scanf("%d %d",&a,&b);
+    printf("\nadd:%d",a+b);
+    printf("\nsub:%d",a-b);
+    printf("\nmul:%d",a*b);
+    printf("\ndiv:%d",a/b);
+    printf("\nmod:%d",a%b);
+}
